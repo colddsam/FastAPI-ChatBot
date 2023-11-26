9eae48daa44e6f62b4b992ea62d916b04b901ca6
colddsam <dassamratkumar772@gmail.com>
2023-11-26 17:36:22 +0530

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import time
+import asyncio
 
 from keras.models import load_model
 import pickle
@@ -9,50 +10,65 @@ from keras.preprocessing.sequence import pad_sequences
 import random
 import json
 
-app=FastAPI()
+app = FastAPI()
 
-def index_mapping(idx):
-    with open('num.pickle', 'rb') as handle:
-        num_index = pickle.load(handle)
+async def load_model_async():
+    return load_model('./training.h5')  # Update with the correct path
+
+async def load_tokenizer_async():
+    with open('./tokenizer.pickle', 'rb') as handle:  # Update with the correct path
+        return pickle.load(handle)
+
+async def load_num_index_async():
+    with open('./num.pickle', 'rb') as handle:  # Update with the correct path
+        return pickle.load(handle)
+
+async def load_dataset_async():
+    with open('./dataset.json', 'r') as outputfile:  # Update with the correct path
+        return json.load(outputfile)
+
+async def get_chatbot_response(user_input):
+    print(user_input)
+    MAX_SEQUENCE_LENGTH = 6
+
+    model = await load_model_async()
+    tokenizer = await load_tokenizer_async()
+    num_index = await load_num_index_async()
+    dataset = await load_dataset_async()
+
+    input_sequence = tokenizer.texts_to_sequences([user_input])
+    padded_sequence = pad_sequences(input_sequence, maxlen=MAX_SEQUENCE_LENGTH)
+    
+    predictions = model.predict(padded_sequence)
+    predicted_intent_index = np.argmax(predictions)
+    idx = index_mapping(predicted_intent_index, num_index)
+    
+    return json_reply(idx, dataset)
+
+def index_mapping(idx, num_index):
     for key, val in num_index.items():
         if val == idx:
             return key
     return 'none'
 
-def json_reply(idx):
-    with open('dataset.json', 'r') as outputfile:
-        dataset = json.load(outputfile)
+def json_reply(idx, dataset):
     replies = []
     for intent in dataset['intents']:
-        if (intent['tag'] == idx):
-            for reply in intent['responses']:
-                replies.append(reply)
+        if intent['tag'] == idx:
+            replies.extend(intent['responses'])
     return random.choice(replies)
 
-
-def get_chatbot_response(user_input):
-    print(user_input)
-    MAX_SEQUENCE_LENGTH = 6
-    model = load_model('training.h5')
-    with open('tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-    input_sequence = tokenizer.texts_to_sequences([user_input])
-    padded_sequence = pad_sequences(input_sequence, maxlen=MAX_SEQUENCE_LENGTH)
-    predictions = model.predict(padded_sequence)
-    predicted_intent_index = np.argmax(predictions)
-    idx = index_mapping(predicted_intent_index)
-
-    return json_reply(idx)
-
 class scoringItem(BaseModel):
-    prompt : str
-    time : str
+    prompt: str
+    time: str
 
 @app.post('/')
-async def scoring_endpoint(item:scoringItem):
-    prompt=item.prompt
-    curr_time=time.strftime("%H:%M:%S" ,time.localtime())
-    reply= get_chatbot_response(prompt)
-    return {"question":prompt,
-            "reply":reply,
-            "time": curr_time}
+async def scoring_endpoint(item: scoringItem):
+    prompt = item.prompt
+    curr_time = time.strftime("%H:%M:%S", time.localtime())
+    
+    try:
+        reply = await get_chatbot_response(prompt)
+        return {"question": prompt, "reply": reply, "time": curr_time}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
